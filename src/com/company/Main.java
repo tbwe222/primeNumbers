b24ee54494006c05be638eac555e5c54f25ed318
Tewodros Wegayehu <tedy_betru@yahoo.com>
2019-04-19 12:31:59 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,6 @@
 package com.company;
 
 import java.util.*;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 public class Main {
 
@@ -14,7 +11,6 @@ public class Main {
         int lastNumber = scan.nextInt();
 
         for (int i = 1; i <= lastNumber; i++) {
-
             if (lastNumber % i/2 == 0) {
                 System.out.println(i + " is a prime number");
             }
